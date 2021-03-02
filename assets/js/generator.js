37e099722f73930dd50f8f40e3a021b1f690d480
John McK <john.mckech3@gmail.com>
2021-03-01 22:21:15 -0600

    initial commit

---
+++ assets/js/generator.js
@@ -0,0 +1,29 @@
+
+
+//button function generates the password
+
+
+
+//prompts for criteria
+//prompt for length between 8-128
+//prompt for character type
+//window.prompt 'Would you like lower case?'
+//if yes then lower case string
+
+function name(params) {
+    
+}
+
+
+var generate = function () {
+
+   
+   
+    /*var promptStart = window.prompt('Pick a password length between 8 and 128 characters');
+    if (promptStart === */
+    
+}
+
+
+
+
